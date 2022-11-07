25aa95cc7094428f63b989921de1df8df1b93ee4
Ronald Alagon <alagon_vra@hotmail.com>
2022-11-07 10:46:22 -0500

    Parte02 Listo por Alagon

--- aplicacion.py
+++ aplicacion.py
@@ -5,11 +5,20 @@ Created on Mon Nov  7 10:08:20 2022
 @author: user
 """
 
-import os
+
+import sqlite3
+
+conexion = sqlite3.connect("CAMAN_ALAGON_NAVARRO_almacen.db")
+
+tabla_producto = """CREATE TABLE producto(idproducto INTEGER PRIMARY KEY AUTOINCREMENT,codigo TEXT UNIQUE,nombre TEXT,precio REAL)"""
+
+cursor = conexion.cursor()
+cursor.execute(tabla_producto)
+
 
 print ("Menu de Opciones \n1. Registrar \n2. Eliminar \n3. Editar \n4. Listar \n5. Salir")
 
 opcion=int(input("Digite la opcion que desea realizar: "))
 
 
-
+conexion.close()
