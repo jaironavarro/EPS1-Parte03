d32ee21540d338e80bdfbbace88a9b1aa8be72fe
Kevin Caman <kevingabriel092001@gmail.com>
2022-11-07 10:21:11 -0500

    Parte01 Lista por Caman

---
+++ aplicacion.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  7 10:08:20 2022
+
+@author: user
+"""
+
+import os
+
+print ("Menu de Opciones \n1. Registrar \n2. Eliminar \n3. Editar \n4. Listar \n5. Salir")
+
+opcion=int(input("Digite la opcion que desea realizar: "))
+
+
+
